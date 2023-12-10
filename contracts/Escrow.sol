e3a7126c5ef65603bcec9482f2b0d8b8ccb27b44
Tomas Jefimovas <t.jefimovas@gmail.com>
2023-12-10 20:00:53 +0200

    initial commit

---
+++ contracts/Escrow.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+interface IERC721 {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _id
+    ) external;
+}
+
+contract Escrow {
+    address payable public seller;
+    address public nftAddress;
+    address public inspector;
+    address public lender; // public lets us access the address outside of the contract
+
+    constructor(
+        address _nftAddress,
+        address payable _seller,
+        address _inspector,
+        address _lender
+    ) {
+        nftAddress = _nftAddress;
+        seller = _seller;
+        inspector = _inspector;
+        lender = _lender;
+    }
+}
